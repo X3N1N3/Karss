8ef59be22dc3996c8a16c8035bd1f5fad369be32
X3N1N3 <74053728+X3N1N3@users.noreply.github.com>
2021-02-09 17:44:04 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,35 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(windowWidth/2-60, 0);
+
+    this.input.position(windowWidth/2-60, windowHeight/2-100);
+    this.button.position(windowWidth/2-60, windowHeight/2-60);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(windowWidth/2-60, 100);
+    });
+
+  }
+}
